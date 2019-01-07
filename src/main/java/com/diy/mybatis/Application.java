fb5d2f669cd4d5a66f035ba4169bcf059e3a8657
ligw <553277890@qq.com>
2019-01-07 12:49:58 +0800

    添加注释

--- src/main/java/com/diy/mybatis/Application.java
+++ src/main/java/com/diy/mybatis/Application.java
@@ -16,17 +16,23 @@ import com.diy.mybatis.session.SqlSessionFactory;
 public class Application {
 
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
-		Field field = System.class.getDeclaredField("props");    
-		field.setAccessible(true);    
-		Properties props = (Properties) field.get(null);    
-		props.put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");    
+  	
+		//创建会话工厂
 		SqlSessionFactory factory=new SqlSessionFactory();
+		//通过工厂生成sqlSession
 		SqlSession sqlSession=factory.openSession();
+		//使用动态代理生成PersonMapper
 		PersonMapper pm=sqlSession.getMapper(PersonMapper.class);
 		List<Person> ps=pm.queryAllPerson();
 		ps.forEach(p->System.out.println(p));
 		System.out.println("################");
 		System.out.println(pm.getPersonById(2));
+		
+		//以下是查看生成代理类的字节码，已经输出到本地，通过反编译工具可以查看
+		Field field = System.class.getDeclaredField("props");    
+		field.setAccessible(true);    
+		Properties props = (Properties) field.get(null);    
+		props.put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");  
 		byte[] proxyClassFile = ProxyGenerator.generateProxyClass(pm.getClass().getName(),pm.getClass().getInterfaces());
 		//将动态代理类生成
 		exportClazzToFile("D:/log1/", pm.getClass().getName()+".class", proxyClassFile);
